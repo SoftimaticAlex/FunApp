305ab72f38c1ffab00a22f19738f04da21e89398
softimatic.alex <softimatic.alex@gmmail.com>
2021-02-26 17:01:37 +0100

    login avaliable

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Home from './src/containers/Home';
 import Chat from './src/containers/Chat';
+import LogIn from './src/containers/LogIn';
 
 const AppNavigator = createStackNavigator({
   Home: { screen: Home },
   Chat: { screen: Chat },
+  LogIn: { screen: LogIn },
 },
 {
-  initialRouteName: 'Home',
+  initialRouteName: 'LogIn',
   headerMode: 'none',
 },
 

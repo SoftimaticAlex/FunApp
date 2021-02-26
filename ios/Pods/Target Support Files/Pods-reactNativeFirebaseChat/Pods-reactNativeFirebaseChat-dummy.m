def354244061fdd5af5aada2902ff4d7e4273300
softimatic.alex <softimatic.alex@gmmail.com>
2021-02-26 16:42:06 +0100

    version estable

---
+++ ios/Pods/Target Support Files/Pods-reactNativeFirebaseChat/Pods-reactNativeFirebaseChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativeFirebaseChat : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativeFirebaseChat
+@end
---
+++ ios/Pods/Target Support Files/Pods-reactNativeFirebaseChatTests/Pods-reactNativeFirebaseChatTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativeFirebaseChatTests : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativeFirebaseChatTests
+@end

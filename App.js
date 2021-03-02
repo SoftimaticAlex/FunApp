4daa4c50aa338188a315a46d032ce8c0a411786b
softimatic.alex <softimatic.alex@gmmail.com>
2021-03-02 16:49:09 +0100

    updated

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import Home from './src/containers/Home';
-import Chat from './src/containers/Chat';
-import LogIn from './src/containers/LogIn';
-import Users from './src/containers/Users';
+import React, { Component } from "react";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Home from "./src/containers/Home";
+import Chat from "./src/containers/Chat";
+import LogIn from "./src/containers/LogIn";
+import Users from "./src/containers/Users";
+import Contacts from "./src/containers/Contacts";
+import IndividualChat from "./src/containers/IndividualChat";
 
-const AppNavigator = createStackNavigator({
-  Home: { screen: Home },
-  Chat: { screen: Chat },
-  LogIn: { screen: LogIn },
-  Users: { screen: Users }
-},
-{
-  initialRouteName: 'LogIn',
-  headerMode: 'none',
-},
-
+const AppNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+    Chat: { screen: Chat },
+    LogIn: { screen: LogIn },
+    Users: { screen: Users, options: Users.navigationOptions },
+    Contacts: { screen: Contacts },
+    IndividualChat: {
+      screen: IndividualChat,
+      options: IndividualChat.navigationOptions,
+    },
+  },
+  {
+    initialRouteName: "LogIn",
+    headerMode: "none",
+  }
 );
 
 const AppContainer = createAppContainer(AppNavigator);
--- src/containers/Users/index.js
+++ src/containers/Users/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   Image,
   SafeAreaView,
@@ -7,21 +7,24 @@ import {
   TouchableOpacity,
   FlatList,
   AsyncStorage,
-} from 'react-native';
+  View,
+  StyleSheet,
+  Button,
+} from "react-native";
 
-import firebase from 'firebase';
-import User from '../User';
+import firebase from "firebase";
+import User from "../User";
 
 export default class HomeScreen extends Component {
-  static navigationOptions = ({navigation, route}) => ({
-    title: 'Chats',
+  static navigationOptions = ({ navigation, route }) => ({
+    title: "Chats",
     headerLeft: null,
     headerRight: () => (
-      <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+      <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
         <Image
-          source={require('../images/user.png')}
+          source={require("../images/user.png")}
           // eslint-disable-next-line react-native/no-inline-styles
-          style={{width: 32, height: 32, marginRight: 10}}
+          style={{ width: 32, height: 32, marginRight: 10 }}
         />
       </TouchableOpacity>
     ),
@@ -29,53 +32,143 @@ export default class HomeScreen extends Component {
 
   state = {
     users: [],
+    usersToFilter: [],
+    ids: [],
   };
 
-  UNSAFE_componentWillMount() {
-    let dbRef = firebase.database().ref('Users');
-    dbRef.on('child_added', val => {
-      let person = val.val();
-      person.phone = val.key;
-      if (person.phone === User.phone) {
-        User.name = person.name;
-      } else {
-        this.setState(prevState => {
-          return {
-            users: [...prevState.users, person],
-          };
+  validateUsersExistence = () => {
+    if (this.state.users.length === 0) return false;
+    return true;
+  };
+
+  validateUser = (key) => {
+    return this.state.usersToFilter.filter((c) => c.phone === key).length > 0;
+  };
+
+  getUserData = (key) => {
+    console.log(this.state.users);
+    return this.state.users.filter((c) => c.phone === key)[0];
+  };
+
+  test = async () => {
+    return await firebase
+      .database()
+      .ref("messages")
+      .child(User.phone)
+      .on("child_added", (res) => {
+        if (this.validateUsersExistence()) {
+          const person = this.getUserData(res.key);
+          if (this.validateUser(res.key)) return;
+
+          if (person) {
+            this.setState((prevState) => {
+              return {
+                ids: [...prevState.ids, res.key],
+                users: [...this.state.users],
+                usersToFilter: [...this.state.usersToFilter, person],
+              };
+            });
+          }
+        } else {
+          this.setState((prevState) => {
+            return {
+              ids: [...prevState.ids, res.key],
+              users: [...this.state.users],
+              usersToFilter: [...this.state.usersToFilter],
+            };
+          });
+        }
+      });
+  };
+
+  usersToFilterHanlder = (person) => {
+    const ids = this.state.ids;
+    if (!ids.includes(person.phone)) return;
+
+    this.setState((prevState) => {
+      return {
+        ids: [...this.state.ids],
+        users: [...this.state.users],
+        usersToFilter: [...this.state.usersToFilter, person],
+      };
+    });
+  };
+
+  componentDidMount() {
+    this.test().then((res) => {
+      firebase
+        .database()
+        .ref("Users")
+        .on("child_added", (val) => {
+          let person = val.val();
+          person.phone = val.key;
+          this.usersToFilterHanlder(person);
+          if (person.phone === User.phone) {
+            User.name = person.name;
+          } else {
+            this.setState((prevState) => {
+              return {
+                users: [...prevState.users, person],
+              };
+            });
+          }
         });
-      }
     });
   }
 
   _logOut = async () => {
     await AsyncStorage.clear();
-    this.props.navigation.navigate('Auth');
+    this.props.navigation.navigate("Auth");
   };
 
-  renderRow = ({item}) => {
+  renderRow = ({ item }) => {
     return (
       <TouchableOpacity
-        onPress={() => this.props.navigation.navigate('Chat', item)}
-        style={{padding: 10, borderBottomColor: '#ccc', borderBottomWidth: 1}}>
-        <Text style={{fontSize: 20}}>{item.name}</Text>
+        onPress={() =>
+          this.props.navigation.navigate({
+            routeName: "IndividualChat",
+            params: item,
+          })
+        }
+        style={{ padding: 10, borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+      >
+        <Text style={{ fontSize: 20 }}>{item.name}</Text>
       </TouchableOpacity>
     );
   };
 
   render() {
     if (!User.phone) {
-      this.props.navigation.navigate('Auth');
+      this.props.navigation.navigate("Auth");
     }
 
     return (
       <SafeAreaView>
+        <View style={styles.position}>
+          <Button
+            onPress={() =>
+              this.props.navigation.navigate({
+                routeName: "Contacts",
+              })
+            }
+            title="Agregar nuevo chat"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
         <FlatList
-          data={this.state.users}
+          data={this.state.usersToFilter}
           renderItem={this.renderRow}
-          keyExtractor={item => item.phone}
+          keyExtractor={(item) => item.phone}
         />
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  position: {
+    display: "flex",
+    flexDirection: "row-reverse",
+  },
+});
